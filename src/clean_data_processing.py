4aa37c899050aaa81c460123af65639ca4dcf377
Michael Zewdu <michaelzewdu00@gmail.com>
2025-06-12 17:42:39 +0300

    Finished up with task_1

--- src/clean_data_processing.py
+++ src/clean_data_processing.py
@@ -1,26 +1,21 @@
-# src\clean_data_processing.py
-import pandas as pd
-
 class cleandataprocessing:
     def __init__(self, df):
         self.df = df
 
-    def clean_missing(self):
-        # Remove columns with more than 50% missing values
+    def clean_missing_values(self):
         limit = len(self.df) * 0.5
         cleaned_df = self.df.dropna(thresh=limit, axis=1).copy()
 
-        # Fill missing numerical values with median, categorical with mode
         for col in cleaned_df.columns:
             if cleaned_df[col].dtype == 'object':
                 mode_val = cleaned_df[col].mode()[0]
-                cleaned_df.loc[:, col] = cleaned_df.loc[:, col].fillna(mode_val)
+                cleaned_df[col] = cleaned_df[col].fillna(mode_val)
             else:
                 median_val = cleaned_df[col].median()
-                cleaned_df.loc[:, col] = cleaned_df.loc[:, col].fillna(median_val)
+                cleaned_df[col] = cleaned_df[col].fillna(median_val)
 
         self.df = cleaned_df
         return cleaned_df
 
-    def no_missing(self):
+    def verify_no_missing_values(self):
         return self.df.isnull().sum().sum() == 0
