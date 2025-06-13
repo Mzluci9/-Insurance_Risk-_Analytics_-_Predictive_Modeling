dc252a1c6097365dc95343a9550289fe8d2e1641
Michael Zewdu <michaelzewdu00@gmail.com>
2025-06-13 15:55:19 +0300

    task_3 first commit

--- src/check_data_quality.py
+++ src/check_data_quality.py
@@ -1,10 +1,19 @@
-def load_data(self):
-    self.df = pd.read_csv(self.path, sep='|', low_memory=False)
-    return self.df
+# src/check_data_quality.py
 
-def basic_info(self):
-    if self.df is not None:
-        print(self.df.info())
-        print(self.df.describe())
-    else:
-        print("Data not loaded yet. Please call the load method first.")
+import pandas as pd
+
+class checkdataquality:
+    def __init__(self, path):
+        self.path = path
+        self.df = None
+
+    def load_data(self):
+        self.df = pd.read_csv(self.path, sep='|', low_memory=False)
+        return self.df
+
+    def basic_info(self):
+        if self.df is not None:
+            print(self.df.info())
+            print(self.df.describe())
+        else:
+            print("Data not loaded yet. Please call the load_data method first.")
